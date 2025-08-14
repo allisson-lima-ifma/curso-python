f2ac056f3f7c28016433144fe5ebdb51304fdfda
Allisson Lima <allysoncosta@outlook.com>
2025-08-13 22:59:21 -0300

    feat: aula 1

---
+++ comandos de saidas.py
@@ -0,0 +1,32 @@
+# A função input() serve para
+# exibir informações na tela, como
+# texto, números e até o resultado
+# de operações.
+print("Hello World!")
+print(123)
+print(3.41)
+
+
+print(1, 2, 3, 4)
+print(2+2)
+print('a' + 'b')
+print('2' + '2')
+
+
+a = 3  # int
+print(a)
+b = 3.2345  # float
+print(b)
+c = "“Qualquer coisa” "  # string
+print(c)
+d = True  # bool
+print(d)
+
+
+# E se eu quiser imprimir o valor
+# de uma variável dentro de uma
+# string ? Podemos tentar:
+a = 123
+print('Testando', a)
+nome = 'Ana'
+print('Olá ' + nome)
